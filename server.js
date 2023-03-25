125efca063f5f726d352896a424cf284eb28b76f
Ramanpreet <rsingh639@myseneca.ca>
2023-03-24 21:05:32 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,29 +1,61 @@
+// cyclic: https://lazy-pear-codfish-hem.cyclic.app/
+// github: https://github.com/Ramanpreet114/Web-test.git
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const pg = require('pg');
 const app = express();
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
+
+const Sequelize = require("sequelize");
 
 
 
-// Configure PostgreSQL connection
-const pool = new pg.Pool({
-    user: 'krhypwqm',
-    host: 'suleiman.db.elephantsql.com',
-    database: 'krhypwqm',
-    password: '0s9wUh-JU1SkxoXswFTv_zGYlicaX2Pp',
-    port: 5432
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+
+function onHttpStart() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+
+
+const sequelize = new Sequelize("ojntptrf", "ojntptrf", "ov4tr4yZAflsgFj8kaexUpfwOxsvz18t", {
+    host: "hansken.db.elephantsql.com",
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+        ssl: { rejectUnauthorized: false }
+    },
+    query: { raw: true }
 });
 
-// Prepare the database
-pool.query(
-    `CREATE TABLE IF NOT EXISTS users (
-           id SERIAL             PRIMARY KEY,
-           name VARCHAR(255)     NOT NULL,
-           email VARCHAR(255)    NOT NULL UNIQUE,
-           created_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP
-        );`
-    );
+
+
+
+
+
+
+let usersTable = sequelize.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true, 
+        autoIncrement: true 
+    },
+    name: Sequelize.STRING,
+    email: Sequelize.STRING,
+    created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    },
+
+    
+},{
+    createdAt: false, 
+    updatedAt: false 
+});
+
 
 // Load styles from public folder
 app.use(express.static("./public/"));
@@ -32,10 +64,14 @@ app.use(express.static("./public/"));
 const hbs = exphbs.create({
     helpers: {
         formatDate: function (date) {
-            return date.toLocaleDateString();
-        }
-    },
-    extname:".hbs"
+            if (date) {
+              return date.toLocaleDateString();
+            } else {
+              return "";
+            }
+          }
+        },
+    extname: ".hbs"
 });
 
 // Register handlebars as the rendering engine for views
@@ -49,90 +85,76 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Serve the HTML form
 app.get('/update-user', (req, res) => {
 
-    const id = req.query.id;
-    pool.query(`SELECT * FROM users WHERE id = ${id}`, (error, results) => {
-        // Handle any errors that occur
-        if (error) {
-            console.error(error);
-            res.status(500).send('Internal server error');
-            return;
-        }
-        // Render the 'index' template with the list of users as a context object
-        res.render('edit', { users: results.rows[0], layout:false });
+
+    usersTable.findOne({
+        where: { id: req.query.id }
+    }).then(data => {
+        res.render('edit', { users: data, layout: false });
     });
-    
+
 });
 
 // Update user data in database
 app.post('/update-user', (req, res) => {
-    const id = req.body.id;
-    const name = req.body.name;
-    const email = req.body.email;
-  
-    // Update user data in database
-    const result = pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email,id],
 
-    (error) => {
-        if (error) {
-            console.log(error); res.status(500).json({ message: 'Error Updating data into PostgreSQL' });
-        } else {
-            res.redirect("/");
-        }
+
+    usersTable.update({
+       
+        name: req.body.name,
+        email: req.body.email
+    }, {
+        where: { id: req.body.id }
+    }).then(() => {
+        console.log("successfully updated name: " + req.body.id);
+        res.redirect("/"); 
     });
-
-  });
+});
 
 // Delete user data in database
 app.get('/delete-user', (req, res) => {
-    const id = req.query.id;
-    // Delete user data from database
-    const result = pool.query(
-    'DELETE FROM users WHERE id = $1',
-    [id],
-    (error) => {
-        if (error) {
-            console.log(error); res.status(500).json({ message: 'Error Delete data from PostgreSQL' });
-        } else {
-            res.redirect("/");
-        }
+
+
+    usersTable.destroy({
+        where: { id: req.query.id }
+    }).then(() => {
+        console.log("Deleted user: " + req.query.id);
+        res.redirect("/"); 
     });
 
-  });
+
+});
 
 // Handle form submission
 app.post('/insert-user', (req, res) => {
-    const { name, email } = req.body;
-    // Insert data into PostgreSQL
-    pool.query(
-        'INSERT INTO users (name, email) VALUES ($1, $2)',
-        [name, email],
-        (error, results) => {
-            if (error) {
-                console.log(error); res.status(500).json({ message: 'Error inserting data into PostgreSQL' });
-            } else {
-                res.redirect("/");
-            }
-        });
+    usersTable.create({
+        name: req.body.name,
+        email: req.body.email
+        
+        }).then(() => {
+        console.log("created a new user");
+        res.redirect("/");
+    });
 });
 
 
 app.get('/', (req, res) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-        // Handle any errors that occur
-        if (error) {
-            console.error(error);
-            res.status(500).send('Internal server error');
-            return;
-        }
-        // Render the 'index' template with the list of users as a context object
-        res.render('index', { users: results.rows, layout:false });
+    
+    usersTable.findAll({
+        attributes1: ["name"],
+        attributes2: ["email"]
+
+    }).then((data) => {
+
+        res.render("index", {
+            data: data,
+            layout: false 
+        });
     });
 });
 
 
-// Start the server
-app.listen(5000, () => {
-    console.log('Server started on http://localhost:5000');
+// synchronize the database before we start the server
+sequelize.sync().then(() => {
+    // start the server to listen on HTTP_PORT
+    app.listen(HTTP_PORT, onHttpStart);
 });
